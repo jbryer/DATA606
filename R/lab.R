27f2949c894cae3b566ce2490907b38cdc1c1f13
Jason Bryer <jason.bryer@gmail.com>
2018-08-25 08:20:45 +0100

    Changed labs to be lowercase.

--- R/lab.R
+++ R/lab.R
@@ -13,7 +13,7 @@
 #' @export
 startLab <- function(l, dest_dir = getwd(),
 					 file.prefix = paste0(Sys.info()['user'], '-')) {
-	path <- paste0(find.package('DATA606'), '/Labs/', l)
+	path <- paste0(find.package('DATA606'), '/labs/', l)
 	file.copy(path, dest_dir, recursive=TRUE, overwrite=FALSE)
 	rmds <- list.files(paste0(dest_dir, '/', l), pattern='.Rmd')
 	new_file <- paste0(dest_dir, '/', l, '/', file.prefix, rmds[1])
@@ -38,7 +38,7 @@ startLab <- function(l, dest_dir = getwd(),
 #' @param l the lab to open.
 #' @export
 viewLab <- function(l) {
-	path <- paste0(find.package('DATA606'), '/Labs/', l)
+	path <- paste0(find.package('DATA606'), '/labs/', l)
 	htmls <- list.files(path, pattern='.html')
 	if(length(htmls) > 0) {
 		system(paste0("open '", path, "/", htmls[1], "'"))
@@ -52,7 +52,7 @@ viewLab <- function(l) {
 #'
 #' @export
 getLabs <- function() {
-	labs <- list.dirs(path=paste0(find.package('DATA606'), '/Labs'),
+	labs <- list.dirs(path=paste0(find.package('DATA606'), '/labs'),
 					  recursive=FALSE, full.names=FALSE)
 	#return(substr(labs, 1, nchar(labs) - 4))
 	return(labs)
