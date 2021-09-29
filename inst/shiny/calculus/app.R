5eb325ca1b2ee8be23afd3025aaa6628e88d7b89
Jason Bryer <jason@bryer.org>
2021-09-29 17:47:28 -0400

    Updated calculus shiny app.

--- inst/shiny/calculus/app.R
+++ inst/shiny/calculus/app.R
@@ -1,16 +1,16 @@
 library(shiny)
 library(tidyverse)
+library(Deriv)
+
+default_fun <- c('Normal' = '1 / (sqrt(2 * pi)) * exp(1)^(-1/2 * (x)^2)',
+				 'Cubic' = '0.25 * x^3 + 0.25 * x^2 + 0.49 * x + 5')
+
+if(FALSE) { # Test
+	f <- eval(parse(text = paste0('f <- function(x) { ', default_fun[1], '}')))
+	f(2)
+}
 
 x_range <- c(-4, 4)
-funs <- list(
-	Normal = function(x, mean = 0, sigma = 1) {
-		1 / (sigma * sqrt(2 * pi)) * exp(1)^(-1/2 * ( (x - mean) / sigma )^2)
-	}
-
-	# Binomial = function(x, trials = 2, p = 0.5) {
-	# 	choose(n, x) * p^x * (1 - p)^(n - x)
-	# }
-)
 
 riemann <- function(f, min, max, n = 2, ...) {
 	width <- (max - min) / n
@@ -23,71 +23,148 @@ riemann <- function(f, min, max, n = 2, ...) {
 }
 
 ui <- fluidPage(
-    titlePanel("Reimann Sums"),
+    titlePanel("Calculus Demo"),
 
     sidebarLayout(
         sidebarPanel(
-        	selectInput('fun',
-        				label = 'Function',
-        				choices = names(funs)),
-        	conditionalPanel(
-        		condition = "input.fun == 'Binomial'",
-        		sliderInput('trials', label = 'Number of trials',
-        					min = 1, max = 100, value = 2),
-        		numericInput('p_success', label = 'Probability of success',
-        					 value = 0.5, min = 0, max = 1, step = 0.1)
+        	selectInput('calc_type', label = 'Choose...', choices = c('Integrals', 'Derivatives')),
+        	selectInput('functions', label = 'Functions', choices = names(default_fun)),
+        	uiOutput('fun'),
+        	uiOutput('min_input'),
+        	uiOutput('max_input'),
+        	hr(),
+        	conditionalPanel(condition = 'input.calc_type == "Integrals"',
+	        	uiOutput('range_input'),
+	        	sliderInput("boxes",
+	        				label = 'Number of boxes',
+	        				min = 1, max = 200,
+	        				step = 1, value = 3)
         	),
-        	sliderInput("range",
-        				label = 'Range',
-        				min = -4, max = 4,
-        				value = c(0, 2),
-        				step = 0.1),
-        	sliderInput("boxes",
-        				label = 'Number of boxes',
-        				min = 1, max = 200,
-        				step = 1, value = 3)
+        	conditionalPanel(condition = 'input.calc_type == "Derivatives"',
+        		uiOutput('point_input'),
+        		numericInput('interval', label = 'Interval',
+        					 min = 0, max = 5, value = 2, step = 0.1)
+			)
         ),
 
         mainPanel(
-           plotOutput("plot"),
-           p('Using the integrate function:', textOutput('area'))
+        	textOutput('error_msg'),
+        	plotOutput("plot"),
+        	conditionalPanel(condition = 'input.calc_type == "Integrals"',
+        					 p('Using the integrate function:', textOutput('area'))
+        	),
+        	conditionalPanel(condition = 'input.calc_type == "Derivatives"'
+        	)
         )
     )
 )
 
 server <- function(input, output) {
-    output$plot <- renderPlot({
-    	width <- abs(diff(input$range) / input$boxes)
-    	df <- NULL
-    	boxes <- NULL
-    	if(input$fun == 'Binomial') {
-    		boxes <- riemann(funs[[input$fun]],
-    						 min = input$range[1],
-    						 max = input$range[2],
-    						 n = input$boxes,
-    						 trials = input$trials,
-    						 p = input$p_success)
-    		df <- tibble(x = seq(0, 3, by = 0.01),
-    					 y = funs[[input$fun]](x, trials = input$trials, p = input$p_success))
-    	} else {
-    		boxes <- riemann(funs[[input$fun]],
-    						 min = input$range[1],
-    						 max = input$range[2],
-    						 n = input$boxes)
-    		df <- tibble(x = seq(x_range[1], x_range[2], by = 0.01),
-	    				 y = funs[[input$fun]](x))
-    	}
-    	ggplot() +
-    		geom_rect(data = boxes, aes(xmin = xmin, ymin = 0, xmax = xmin + width, ymax = height),
-    				  alpha = 0.5, color = 'black') +
-    		geom_path(data = df, aes(x = x, y = y)) +
-    		xlim(c(x_range[1], x_range[2])) +
-    		ggtitle(paste0('Aera ≈ ', prettyNum(sum(boxes$area)), digits = 4))
+	error_message <- reactiveVal('')
 
+	output$error_msg <- renderText({
+		error_message()
+	})
+
+	getFunction <- reactive({
+		tryCatch({
+			eval(parse(text = paste('f <- function(x) { return(' , input$fun , ')}', sep='')))
+			error_message('')
+		}, error = function(e) {
+			print(e)
+			error_message('Error in function specification.')
+		})
+		return(f)
+	})
+
+	output$min_input <- renderUI({
+		numericInput('view_min', label = 'Plot min', value = -4, step = 1,
+					 min = -100, max = 0)
+	})
+
+	output$max_input <- renderUI({
+		numericInput('view_max', label = 'Plot max', value = 4, step = 1,
+					 min = 0, max = 100)
+	})
+
+	output$range_input <- renderUI({
+		req(input$view_min); req(input$view_max)
+		sliderInput("range",
+					label = 'Range',
+					min = input$view_min, max = input$view_max,
+					value = c(0, 2),
+					step = 0.1)
+	})
+
+	output$point_input <- renderUI({
+		req(input$view_min); req(input$view_max)
+		sliderInput('point',
+					label = 'Point',
+					value = input$view_min + (input$view_max - input$view_min) / 4,
+					min = input$view_min,
+					max = input$view_max,
+					step = 0.2)
+	})
+
+	output$fun <- renderUI({
+		textInput('fun', label = 'Function', value = default_fun[input$functions])
+	})
+
+	output$plot <- renderPlot({
+		req(input$view_min); req(input$view_max)
+		f <- getFunction()
+		if(is.null(f)) { return(); }
+
+		p <- NULL
+		if(input$calc_type == 'Integrals') {
+			req(input$range); req(input$boxes)
+
+	    	width <- abs(diff(input$range) / input$boxes)
+	    	boxes <- riemann(f,
+							 min = input$range[1],
+							 max = input$range[2],
+							 n = input$boxes)
+	    	p <- ggplot() +
+	    		geom_rect(data = boxes, aes(xmin = xmin, ymin = 0, xmax = xmin + width, ymax = height),
+	    				  alpha = 0.5, color = 'black') +
+	    		stat_function(fun = f) +
+	    		xlim(c(input$view_min, input$view_max)) +
+	    		ggtitle(paste0('Aera ≈ ', prettyNum(sum(boxes$area)), digits = 4))
+		} else if(input$calc_type == 'Derivatives') {
+			req(input$point); req(input$interval)
+
+			# dx <- Deriv(f, "x") # TODO: Why doesn't this work!
+
+			df_segment <- data.frame(x1 = input$point - input$interval,
+									 x2 = input$point + input$interval,
+									 y1 = f(input$point - input$interval),
+									 y2 = f(input$point + input$interval) )
+
+			segment_slope <- (df_segment$y2 - df_segment$y1) / (df_segment$x2 - df_segment$x1)
+
+			p <- ggplot() +
+				stat_function(fun = f) +
+				# geom_abline(slope = dx(x)[1], intercept = f(x) - dx(x)[1] * x, color = 'blue') +
+				geom_segment(data = df_segment,
+							 aes(x = x1, y = y1, xend = x2, yend = y2),
+							 color = 'darkgreen', linetype = 2) +
+				geom_abline(slope = segment_slope,
+							intercept = f(input$point) - segment_slope * input$point,
+							color = 'darkgreen') +
+				geom_point(aes(x = df_segment$x1, y = df_segment$y1), color = 'darkgreen', size = 2) +
+				geom_point(aes(x = df_segment$x2, y = df_segment$y2), color = 'darkgreen', size = 2) +
+				geom_point(aes(x = input$point, y = f(input$point)), color = 'blue', size = 3) +
+				xlim(c(input$view_min, input$view_max)) +
+				ggtitle(paste0('Slope ≈ ', segment_slope))
+		}
+		return(p)
     })
 
     output$area <- renderPrint({
-    	integrate(funs[[input$fun]], input$range[1], input$range[2])
+    	req(input$range)
+    	f <- getFunction()
+    	if(is.null(f)) { return(); }
+    	integrate(f, input$range[1], input$range[2])
     })
 }
 
